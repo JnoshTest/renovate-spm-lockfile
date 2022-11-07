8f77bc6c14d6698b8474d5ce1ca99b530412fbed
Janosch Hildebrand <jnosh@jnosh.com>
2022-11-07 17:07:21 +0100

    Add additional test packages to test swift tools version extraction

---
+++ package4/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "test1",
+    platforms: [
+       .macOS(.v12)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-system", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-async-algorithms", from: "0.0.1"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "Run",
+            dependencies: [
+                .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
+                .product(name: "SystemPackage", package: "swift-system"),
+            ]
+        )
+    ]
+)
